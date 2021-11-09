df9b45641164f0fe52c8d4a74f2b011ac4d2a6ae
roblesdotdev <robles.contactme@gmail.com>
2021-11-09 14:08:26 -0300

    control flow

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { ref } from "vue";
 const name = "World";
+const ages = [32, 23, 39, 19];
 
 const count = ref(0);
+
+function isEven() {
+  return count.value % 2 === 0;
+}
 </script>
 
 <template>
@@ -11,6 +16,12 @@ const count = ref(0);
   <button @click="count++">Increment</button>
 
   <h3>{{ count }}</h3>
+  <p v-if="isEven()">Even</p>
+  <p v-else>Odd</p>
+
+  <div v-for="age in ages">
+    {{ age }}
+  </div>
 </template>
 
 <style></style>
