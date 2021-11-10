d053370654027816ebc2b1386abd8a997f32edf8
roblesdotdev <robles.contactme@gmail.com>
2021-11-10 10:04:43 -0300

    working with v-model

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <script setup>
 import { ref } from "vue";
-const name = "World";
-const ages = [32, 23, 39, 19];
-
-const count = ref(0);
-
-function isEven() {
-  return count.value % 2 === 0;
-}
-
-function getClass(age) {
-  if (age % 2 === 0) {
-    return "red";
-  } else {
-    return "blue";
-  }
-}
+const name = ref("World");
 </script>
 
 <template>
-  <h1>Hello {{ name }}!</h1>
-
-  <button @click="count++">Increment</button>
-
-  <h3>{{ count }}</h3>
-  <p v-if="isEven()">Even</p>
-  <p v-else>Odd</p>
-
-  <div v-for="age in ages">
-    <p v-bind:class="getClass(age)">{{ age }}</p>
-  </div>
+  <input v-model="name" />
+  <p>Hello {{ name }}!</p>
 </template>
 
 <style>
